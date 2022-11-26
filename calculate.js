c098e05a768cf1293b205be612def48d64a5f2c8
chinnawich <hotlnw159@gmail.com>
2022-11-26 09:34:39 +0700

    JavaScript

---
+++ calculate.js
@@ -0,0 +1,5 @@
+module.exports =  function (a,x,n) {
+    if(x===1) return a*n;
+    return a + x
+}
+
--- index3.js
+++ index3.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const os = require('os')
+const http = require("http")
 
 const student = [
     {
@@ -19,6 +21,7 @@ const student = [
     },
 ]
 
+//WRITE FILE
 // fs.writeFileSync(
 //   path.join(__dirname, "user.json"),
 //   JSON.stringify(student),
@@ -30,16 +33,43 @@ const student = [
 //   }
 // );
 
+// READ FILE
+// fs.readFile(path.join(__dirname, 'user.json'), (err, data)=> {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     const users = JSON.parse(data);
 
-fs.readFile(path.join(__dirname, 'user.json'), (err, data)=> {
-    if(err) {
-        console.log(err);
-        return;
-    }
-    const users = JSON.parse(data);
+//     console.log(users)
+//     for (const user of users) {
+//         console.log(user.id)
+//     }
+// })
 
-    console.log(users)
-    for (const user of users) {
-        console.log(user.id)
-    }
-})
+//Operation System (OS)
+
+// console.log(`Hostname: ${os.hostname()}`)
+// console.log(`CPU architecture: ${os.arch()}`)
+// console.log("Total memory: " + (os.totalmem()/1e6.toFixed(1)));
+// console.log("Free memory: " + (os.freemem()/1e6.toFixed(1)));
+
+// CREATE SERVER
+
+// const server = http.createServer(function(res, res) {
+//     console.log(`${req.method} ${req.url}`)
+//     res.end('Hello world!')
+// })
+
+// const port = 8080;
+
+// server.listen(port, ()=> {
+//     console.log(`server started on port ${port}`)
+// })
+
+
+
+// REQUIRE MODULE 
+// const test_calculate = require('./calculate.js')
+
+// console.log(test_calculate(1,2,3))
